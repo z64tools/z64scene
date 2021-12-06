385bcef216c93684fa778a67c2fa0ca3911e06a9
z64me <me@z64.me>
2021-12-05 19:13:01 -0500

    cleanup

--- include/Global.h
+++
@@ -1,8 +0,0 @@
-#ifndef __Z64GLOBAL_H__
-#define __Z64GLOBAL_H__
-
-struct GlobalContext;
-
-#define ACTIVE_CAM globalCtx->view.currentCamera
-
-#endif
--- include/zroom.h
+++
@@ -1,6 +0,0 @@
-#ifndef Z64_ZROOM_H_INCLUDED
-#define Z64_ZROOM_H_INCLUDED
-
-void zroom_draw(void* room);
-
-#endif
