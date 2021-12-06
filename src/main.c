18a34c049bf43baf661682838099f79a4b9e42c9
z64me <me@z64.me>
2021-12-05 20:15:30 -0500

    update `z64viewer` submodule, add `nanovg` gui

--- src/main.c
+++ src/main.c
@@ -1,4 +1,10 @@
 #include <z64viewer.h>
+#include <nanovg.h>
+#define NANOVG_GL3_IMPLEMENTATION
+#include <nanovg_gl.h>
+
+static NVGcontext* vg = NULL;
+static int gFont = -1;
 
 typedef struct drawParams
 {
@@ -8,10 +14,37 @@ typedef struct drawParams
 
 static void draw(void *udata)
 {
+	int windowWidth;
+	int windowHeight;
+	
 	DrawParams *params = udata;
 	
 	z64viewer_scene(params->zScene.data);
 	z64viewer_room(params->zRoom.data);
+	
+	z64viewer_get_windowDimensions(&windowWidth, &windowHeight);
+	float pxRatio = (float)windowWidth / (float)windowHeight;
+	nvgBeginFrame(vg, windowWidth, windowHeight, pxRatio);
+	
+	nvgBeginPath(vg);
+	nvgRect(vg, 100,100, 150,30);
+	nvgFillColor(vg, nvgRGBA(255,192,0,255));
+	nvgFill(vg);
+	if (gFont != -1)
+	{
+		nvgFillColor(vg, nvgRGBA(0,0,0,220));
+		nvgFontSize(vg, 15.0f);
+		nvgFontFace(vg, "sans");
+		nvgTextAlign(vg, NVG_ALIGN_LEFT | NVG_ALIGN_TOP);
+		nvgTextBox(vg, 100,100, 150, "wow, hermosauhuts everywhere!", NULL);
+	}
+
+	//renderGraph(vg, 5,5, &fps);
+	//renderGraph(vg, 5+200+5,5, &cpuGraph);
+	//if (gpuTimer.supported)
+	//	renderGraph(vg, 5+200+5+200+5,5, &gpuGraph);
+
+	nvgEndFrame(vg);
 }
 
 int main(void)
@@ -25,8 +58,19 @@ int main(void)
 	MemFile_LoadFile(&params.zScene, "scene.zscene");
 	MemFile_LoadFile(&params.zRoom, "room_0.zmap");
 	
-	if (z64viewer_init("z64viewer"))
+	if (z64viewer_init("z64scene"))
 		return 0;
+	
+#ifdef DEMO_MSAA
+	vg = nvgCreateGL3(NVG_STENCIL_STROKES | NVG_DEBUG);
+#else
+	vg = nvgCreateGL3(NVG_ANTIALIAS | NVG_STENCIL_STROKES | NVG_DEBUG);
+#endif
+	if (vg == NULL) {
+		printf("Could not init nanovg.\n");
+		return -1;
+	}
+	gFont = nvgCreateFont(vg, "sans", "NotoSans-Regular.ttf");
 	
 	// render loop
 	// -----------
