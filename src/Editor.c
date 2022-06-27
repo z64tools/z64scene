b69fd9b85dd1c066a710fce604e063488cbae94d
rankaisija64 <65511061+rankaisija64@users.noreply.github.com>
2022-06-27 14:32:41 +0300

    Prepare Editing Functions

--- src/Editor.c
+++ src/Editor.c
@@ -1,5 +1,48 @@
 #include "Editor.h"
 
+// # # # # # # # # # # # # # # # # # # # #
+// # EDITING                             #
+// # # # # # # # # # # # # # # # # # # # #
+
+ActorEntry* Actor_Add(Room* room, u16 id, u16 param, Vec3s pos, Vec3s rot) {
+	ActorEntry* newActor = &room->actor[room->actorNum++];
+	
+	newActor->index = id;
+	newActor->param = param;
+	newActor->pos = pos;
+	newActor->rot = rot;
+	
+	return newActor;
+}
+
+void Actor_Delete(Room* room, s32 actorIndex) {
+	for (s32 i = actorIndex; i < room->actorNum; i++) {
+		room->actor[i] = room->actor[1 + i];
+	}
+	
+	room->actorNum--;
+}
+
+ObjectEntry* Object_Add(Room* room, u16 id) {
+	ObjectEntry* newObj = &room->object[room->objectNum++];
+	
+	newObj->index = id;
+	
+	return newObj;
+}
+
+void Object_Delete(Room* room, s32 objIndex) {
+	for (s32 i = objIndex; i < room->objectNum; i++) {
+		room->object[i] = room->object[1 + i];
+	}
+	
+	room->objectNum--;
+}
+
+// # # # # # # # # # # # # # # # # # # # #
+// # EDITOR                              #
+// # # # # # # # # # # # # # # # # # # # #
+
 void Editor_DropCallback(GLFWwindow* window, s32 count, char* item[]) {
 }
 
--- src/Editor.h
+++ src/Editor.h
@@ -1,13 +1,46 @@
 #include <ExtLib.h>
 #include <ExtGui/Global.h>
 
+typedef struct {
+	u16   index;
+	u16   param;
+	Vec3s pos;
+	Vec3s rot;
+} ActorEntry;
+
+typedef struct {
+	u16 index;
+} ObjectEntry;
+
+typedef struct {
+	u16 actorNum;
+	u16 objectNum;
+	MemFile     mesh;
+	ActorEntry  actor[255];
+	ObjectEntry object[255];
+} Room;
+
+typedef struct {
+	s32 curRoom;
+	
+	MemFile file;
+	Room    room[64];
+} Scene;
+
 typedef struct EditorContext {
 	AppInfo app;
 	void*   vg;
 	GeoGrid geoGrid;
 	CursorContext cursor;
 	InputContext  input;
+	
+	Scene scene;
 } EditorContext;
+
+ActorEntry* Actor_Add(Room* room, u16 id, u16 param, Vec3s pos, Vec3s rot);
+void Actor_Delete(Room* room, s32 actorIndex);
+ObjectEntry* Object_Add(Room* room, u16 id);
+void Object_Delete(Room* room, s32 objIndex);
 
 void Editor_DropCallback(GLFWwindow* window, s32 count, char* item[]);
 void Editor_Update(EditorContext* editor);
